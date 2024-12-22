d38bf433d4c65cebced642efd815f997d55d423b
raidBer <Kr_berrahal@esi.dz>
2024-12-22 17:55:30 +0100

    added logout button, added routes to the landing page

--- app/Views/landing.php
+++ app/Views/landing.php
@@ -13,17 +13,24 @@
         <div class="left-section">
             <h1>Mettez vos méninges à l'épreuve !</h1>
             <p>Découvrez une activité classique et captivante qui stimule votre esprit tout en vous amusant. Complétez les grilles en trouvant les mots qui correspondent aux indices donnés.</p>
-            <div class="buttons">
-                <a href="../public/index.php?action=signup" class="button create-account-button">
-                    Créez un compte
-                    <img src="../public/images/right-arrow.png" alt="Arrow Icon">
-                </a>
-                <a href="../public/index.php?action=login" class="button login-button">
-                    Se connecter
-                    <img src="../public/images/right-arrow.png" alt="Arrow Icon">
-                </a>
-            </div>
-            <a href="games.php" class="button play-button">Jouez dès maintenant !</a>
+            
+            <?php
+                // Vérifier si 'username' est dans $_POST et non vide
+                if (!isset($_SESSION['username'])) {
+                    echo '<div class="buttons">
+                        <a href="../public/index.php?action=signup" class="button create-account-button">
+                            Créez un compte
+                            <img src="../public/images/right-arrow.png" alt="Arrow Icon">
+                        </a>
+                        <a href="../public/index.php?action=login" class="button login-button">
+                            Se connecter
+                            <img src="../public/images/right-arrow.png" alt="Arrow Icon">
+                        </a>
+                    </div>';
+                } 
+            ?>
+                
+            <a href="../public/index.php?action=grids" class="button play-button">Jouez dès maintenant !</a>
         </div>
         <div class="right-section">
             <img src="../public/images/home.png" alt="Image" class="landing-image">
